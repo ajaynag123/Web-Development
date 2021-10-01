42a72e761616df5bb3fcf8b6901f6ab8a59f6979
KARANAM AJAY NAG <ajaynagkaranam@gmail.com>
2021-10-01 10:22:54 +0530

    My first React App

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,11 +1,46 @@
 import React from "react";
 
-const title = 'React';
+const welcome = {
+  greeting : 'Hey',
+  title : 'React',
+};
+
+function getTitle(title){
+  return title;
+}
+
+const array = ['bat','ball']
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>My Hacker Stories</h1>
+      <label htmlFor='search'>Search:</label>
+      <input id = 'search' type = 'text'/>
+      <hr/>
+      {list.map(function (item) {
+        return <div>{item.title}</div>
+      }
+      )}
     </div>
   );
 }
