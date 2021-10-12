e7b1d6d63550cbba7adec15d6593c3ec90321ae1
KARANAM AJAY NAG <ajaynagkaranam@gmail.com>
2021-10-12 13:55:10 +0530

    commit

--- React/react-complete guide/src/components/Expenses/ExpenseItems.js
+++ React/react-complete guide/src/components/Expenses/ExpenseItems.js
@@ -1,17 +1,18 @@
 import "./ExpenseItems.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from '../UI/Card';
 
-function ExpenseItems(props) {
+const ExpenseItems = (props) => {
  
  
  return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2 className='expense-item'>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
---
+++ React/react-complete guide/src/components/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import ExpenseItems  from "./ExpenseItems";
+import './Expenses.css';
+import Card from '../UI/Card';
+
+const Expenses = (props) => {
+    return (
+        <Card className='expenses'>
+            <ExpenseItems
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+        date={props.expenses[0].date}
+      ></ExpenseItems>
+      <ExpenseItems
+        title={props.expenses[1].title}
+        amount={props.expenses[1].amount}
+        date={props.expenses[1].date}
+      ></ExpenseItems>
+      <ExpenseItems
+        title={props.expenses[2].title}
+        amount={props.expenses[2].amount}
+        date={props.expenses[2].date}
+      ></ExpenseItems>
+      <ExpenseItems
+        title={props.expenses[3].title}
+        amount={props.expenses[3].amount}
+        date={props.expenses[3].date}
+      ></ExpenseItems>
+        </Card>
+    )
+}
+
+
+export default Expenses;
