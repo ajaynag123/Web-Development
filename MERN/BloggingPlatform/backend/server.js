be524efeda4993ef433daa51d50e93c7f7d49a8a
KARANAM AJAY NAG <ajaynagkaranam@gmail.com>
2021-12-07 20:08:58 +0530

    commit

---
+++ MERN/BloggingPlatform/backend/server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+require('dotenv').config();
